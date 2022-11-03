3976083049f0c7627a37e2f8691d08b1e7f7fc84
William <p23wclegg@fairfieldprep.org>
2022-11-03 11:12:01 -0400

    dheheeh

---
+++ Lab_6-1.py
@@ -0,0 +1,22 @@
+#Lab_6-1
+
+from operator import index
+
+
+my_list= ['1', '2', '3', '4', '5']
+#making a list with numbers 1 through 5
+
+my_second_list= index, my_list[2]
+print (my_second_list)
+#Making a new variable that is only the 3rd number in my first list and printing
+
+Lenght_of_list= len(my_list)
+print(Lenght_of_list)
+#New print for how ong the list is 
+
+print(my_list*3)
+#Print statement to print the list 3 times
+
+string="string"
+print(list(string))
+#making a print statement that makes the word string into a list
