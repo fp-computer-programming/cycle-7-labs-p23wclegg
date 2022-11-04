eea7d1f513139a4f61dbe21ecad47ff5360f4ab9
William <p23wclegg@fairfieldprep.org>
2022-11-04 11:18:02 -0400

    fdafsd

---
+++ Lab_6-2.py
@@ -0,0 +1,17 @@
+#Lab_6-2
+
+
+my_row=['Ben', 'Brian']
+#setting my row equal to the list of people in my orw
+
+my_row[1]="Will"
+print(my_row)
+#replacing the second name in the list with my own
+
+my_row_2=my_row
+print(my_row_2)
+#setting both row variables equal to each other 
+
+del my_row[1]
+print (my_row)
+#deleting the second term in the first row variable, it only prints the first term in the list for the first variable
